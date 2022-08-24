176c99349c7bc7e25425dd69d27763fe1bcf327d
Neto <ukpongneto@wellahealth.com>
2022-08-24 15:41:39 +0100

    cors headers set in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-// const cors = require('cors')
+const cors = require('cors')
 // const authJwt = require('./utils/jwt')
 require('dotenv/config')
 
@@ -10,22 +10,40 @@ const app = express()
 
 const port = process.env.PORT || 3550
 
+// allow all config for cors
+// app.use(cors())
+// app.options('*', cors())
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    // Request methods you wish to allow
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    )
+
+    // Request headers you wish to allow
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,content-type'
+    )
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    // Pass to next layer of middleware
+    next()
+})
+
 // routes
 const userRoutes = require('./routes/users')
 const requestRoutes = require('./routes/requests')
 const realEstateRoutes = require('./routes/realEstate')
 const referenceRoutes = require('./routes/references')
 const contactRoutes = require('./routes/contacts')
-
-// allow all config for cors
-// var corsOptions = {
-//     origin: '*',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204,
-// }
-// app.use(cors(corsOptions))
-// app.options('*', cors())
 
 // middleware
 app.use(bodyParser.json())
@@ -40,9 +58,6 @@ app.use('/api/requests', requestRoutes)
 app.use('/api/real-estate', realEstateRoutes)
 app.use('/api/reference', referenceRoutes)
 app.use('/api/contact', contactRoutes)
-
-// proxy setup
-app.set('trust proxy', 1)
 
 // connect to database
 mongoose
